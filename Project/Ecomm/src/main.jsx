6cf9205bf8b508fd64a8fda344e4e6c93e585b7b
chintan <chintan.tops@gmail.com>
2024-12-13 08:58:32 +0530

    api

--- Project/Ecomm/src/main.jsx
+++ Project/Ecomm/src/main.jsx
@@ -5,22 +5,32 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './assets/css/style.css'
 import './assets/css/style.min.css'
 import 'font-awesome/css/font-awesome.min.css';
-import {RouterProvider,createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Products from './components/Products.jsx'
 import Shop from './components/Shop.jsx'
 import Contact from './components/Contact.jsx'
 import Cart from './components/Cart.jsx'
 import ShopDetail from './components/ShopDetail.jsx'
+import Registration from './components/Registration.jsx'
+import Login from './components/Login.jsx'
 
-const router =   createBrowserRouter([
-    {path:"/", element:<App/>,
-       children:[{path:'/', element:<Products/>},
-        {path:'/shop', element:<Shop/>},
-        {path:'/contact', element:<Contact/>},
-        {path:'/cart',element:<Cart/>},
-        {path:'/details', element:<ShopDetail/>}
-       ]},
-   
+const router = createBrowserRouter([
+  {
+    path: "/", element: <App />,
+    children: [{ path: '/', element: <Products /> },
+    { path: '/shop', element: <Shop /> },
+    { path: '/contact', element: <Contact /> },
+    { path: '/cart', element: <Cart /> },
+    { path: '/details', element: <ShopDetail /> }
+    ]
+  },
+  {
+    path: "/login", element: <Login/>
+  },
+  {
+    path: "/reg", element: <Registration />
+  }
+
 ])
 
 createRoot(document.getElementById('root')).render(
