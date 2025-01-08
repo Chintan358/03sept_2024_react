f906866e94b484e6073a21f1290053ef102d0199
chintan <chintan.tops@gmail.com>
2025-01-08 08:46:45 +0530

    firebas ecrud

--- Firebase-crud/src/App.jsx
+++ Firebase-crud/src/App.jsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react"
 import AddUser from "./components/AddUser"
 import ViewUser from "./components/ViewUser"
 import { db } from './components/firebase'
-import { collection, addDoc, getDocs, doc, deleteDoc } from 'firebase/firestore'
+import { collection, addDoc, getDocs, doc, deleteDoc, getDoc, updateDoc } from 'firebase/firestore'
 
 
 
 const App = () => {
 
   const [users, setUsers] = useState([])
+  const [user, setuser] = useState({})
+  const [loading, setLoading] = useState(false)
 
   const userRefCollection = collection(db, "users")
 
@@ -20,12 +22,14 @@ const App = () => {
   }, [])
 
   const getData = async () => {
+    setLoading(true)
     setUsers([])
     const allusers = await getDocs(userRefCollection)
 
     var newUser = allusers.docs.map(ele => {
 
       setUsers(currentuser => [...currentuser, { id: ele.id, uname: ele.data().uname, email: ele.data().email, phone: ele.data().phone }])
+      setLoading(false)
     })
 
   }
@@ -34,11 +38,19 @@ const App = () => {
 
   const addUserHandlerFunction = async (userdata) => {
 
-    await addDoc(userRefCollection, userdata)
+    if (userdata.id) {
+      await updateDoc(doc(db, "users", userdata.id), { uname: userdata.uname, email: userdata.email, phone: userdata.phone })
+    }
+    else {
+      await addDoc(userRefCollection, userdata)
+    }
     getData()
   }
 
   const editUser = async (id) => {
+
+    const result = await getDoc(doc(db, "users", id))
+    setuser({ id: result.id, uname: result.data().uname, email: result.data().email, phone: result.data().phone })
 
   }
 
@@ -48,12 +60,15 @@ const App = () => {
     getData()
   }
 
+
+
+
   return <>
     <div className="container">
       <div className="row">
-        <AddUser addUserHandlerFunction={addUserHandlerFunction}></AddUser>
+        <AddUser addUserHandlerFunction={addUserHandlerFunction} user={user}></AddUser>
         <div className="col-1"></div>
-        <ViewUser users={users} editUser={editUser} deleteUser={deleteUser}></ViewUser>
+        <ViewUser users={users} editUser={editUser} deleteUser={deleteUser} loading={loading}></ViewUser>
       </div>
     </div>
   </>
